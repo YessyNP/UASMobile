d9e528b886af85466f84ac8309be5ba367b740f0
YessyNP <yessynindi@gmail.com>
2019-12-19 10:23:25 +0700

    Mmebuat Login Activity dan mengubah isi activity_main

---
+++ app/src/main/java/com/yessy/uasmobile/activities/LoginActivity.java
@@ -0,0 +1,20 @@
+package com.yessy.uasmobile.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import android.os.Bundle;
+
+
+import com.yessy.uasmobile.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+
+    }
+}
