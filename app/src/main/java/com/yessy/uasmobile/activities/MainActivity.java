90ad6a30edd40eb9a56adf3be2f6d3e91ac264c7
YessyNP <yessynindi@gmail.com>
2019-12-20 10:24:35 +0700

    mengubah isi activity_login

--- app/src/main/java/com/yessy/uasmobile/activities/MainActivity.java
+++ app/src/main/java/com/yessy/uasmobile/activities/MainActivity.java
@@ -2,15 +2,23 @@ package com.yessy.uasmobile.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.yessy.uasmobile.R;
 
-public class MainActivity extends AppCompatActivity {
+    public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-}
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+        }
+
+        public void Login(View view) {
+
+                Intent intent = new Intent(this, LoginActivity.class);
+                startActivity(intent);
+            }
+        }
