25d6d54cf289d719832dbf5604cfd798ccd26e20
YessyNP <yessynindi@gmail.com>
2019-12-20 22:58:39 +0700

    menambahkan menu navigation dan membuat fragment about,diskon,dan regulation

--- app/src/main/java/com/yessy/uasmobile/activities/MainActivity.java
+++ app/src/main/java/com/yessy/uasmobile/activities/MainActivity.java
@@ -1,20 +1,64 @@
 package com.yessy.uasmobile.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import androidx.fragment.app.Fragment;
 import com.yessy.uasmobile.R;
+import com.yessy.uasmobile.fragments.AboutFragment;
+import com.yessy.uasmobile.fragments.DiscountFragment;
+import com.yessy.uasmobile.fragments.RegulationFragment;
 
-    public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
 
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_main);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        loadFragment(new AboutFragment());
+        BottomNavigationView bottomNavigationView = findViewById(R.id.navigation);
+        // beri listener pada saat item/menu bottomnavigation terpilih
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+    }
+
+    private boolean loadFragment(Fragment fragment) {
+        if (fragment != null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .addToBackStack(null)
+                    .commit();
+            return true;
         }
+        return false;
+    }
 
-            }
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        Fragment fragment = null;
+        switch (menuItem.getItemId()) {
+            case R.id.action_food:
+                fragment = new AboutFragment();
+                break;
+            case R.id.action_regulation:
+                fragment = new RegulationFragment();
+                break;
+            case R.id.action_discount:
+                fragment = new DiscountFragment();
+                break;
+        }
+        return loadFragment(fragment);
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
+    }
+}
 
 
 
