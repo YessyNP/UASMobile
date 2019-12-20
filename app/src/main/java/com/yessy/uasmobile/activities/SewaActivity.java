5bbbd128471848408d58a878aa1fd73961ce782a
YessyNP <yessynindi@gmail.com>
2019-12-20 23:05:29 +0700

    mengaktifkan button about pada Sewa Activity

--- app/src/main/java/com/yessy/uasmobile/activities/SewaActivity.java
+++ app/src/main/java/com/yessy/uasmobile/activities/SewaActivity.java
@@ -2,9 +2,12 @@ package com.yessy.uasmobile.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.yessy.uasmobile.R;
+import com.yessy.uasmobile.fragments.AboutFragment;
 
 public class SewaActivity extends AppCompatActivity {
 
@@ -12,5 +15,10 @@ public class SewaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sewa);
+    }
+
+    public void handleAbout(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
