432980e1b26b643dbb0b074a3a3931ff644226da
YessyNP <yessynindi@gmail.com>
2019-12-22 10:25:20 +0700

    membuat SepatuActivity

---
+++ app/src/main/java/com/yessy/uasmobile/activities/SepatuActivity.java
@@ -0,0 +1,16 @@
+package com.yessy.uasmobile.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.yessy.uasmobile.R;
+
+public class SepatuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sepatu);
+    }
+}
