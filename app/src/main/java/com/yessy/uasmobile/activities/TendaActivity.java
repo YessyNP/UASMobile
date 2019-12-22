f3053a3c01c4a7d1312b1cbb634f494034bbcc79
YessyNP <yessynindi@gmail.com>
2019-12-22 10:21:03 +0700

    membuat TendaActivity

---
+++ app/src/main/java/com/yessy/uasmobile/activities/TendaActivity.java
@@ -0,0 +1,16 @@
+package com.yessy.uasmobile.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.yessy.uasmobile.R;
+
+public class TendaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tenda);
+    }
+}
